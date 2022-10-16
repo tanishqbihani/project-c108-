b4630296954944980ce7ad166467de1779c14003
tanishqbihani <87018616+tanishqbihani@users.noreply.github.com>
2022-10-16 15:45:50 +0530

    Update sign_language.py

--- sign_language.py
+++ sign_language.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pyautogui
+import imutils
 import cv2
 import mediapipe as mp
 
@@ -22,47 +25,42 @@ while True:
             lm_list=[]
             for id ,lm in enumerate(hand_landmark.landmark):
                 lm_list.append(lm)
-    
-            finger_fold_status=[]
-            
+
+            #array to hold true or false if finger is folded    
+            finger_fold_status =[]
             for tip in finger_tips:
+                #getting the landmark tip position and drawing blue circle
                 x,y = int(lm_list[tip].x*w), int(lm_list[tip].y*h)
-                cv2.circle(img, (x,y), 15, (255, 0 , 0), cv2.FILLED)
+                cv2.circle(img, (x,y), 15, (255, 0, 0), cv2.FILLED)
 
-    #if results.finger_fold_status:
-        #for fold_status in results.finger_fold_status:
-            if lm_list[tip].x < lm_list[tip - 3].x:
-                cv2.circle(img, (x,y), 15, (255, 0 , 0), cv2.FILLED)
-                finger_fold_status.append(True)
-            else:
-                finger_fold_status.append(False)
+                #writing condition to check if finger is folded i.e checking if finger tip starting value is smaller than finger starting position which is inner landmark. for index finger    
+                #if finger folded changing color to green
+                if lm_list[tip].x < lm_list[tip - 3].x:
+                    cv2.circle(img, (x,y), 15, (0, 255, 0), cv2.FILLED)
+                    finger_fold_status.append(True)
+                else:
+                    finger_fold_status.append(False)
 
-        print(finger_fold_status)    
-        for lm_index in tipids:
-            #get finger tip and bottom y position value 
-            finger_tip_y = landmarks[lm_index].y
-            finger_bottom_y = landmarks[lm_index -2].y
-            
-            #check if any finger is open or closed
-            if lm_index !=4:
-                if finger_tip_y < finger_bottom_y:
-                    fingers.append(1)
-                    print("Fingers with id ",lm_index,"is Open")
+            print(finger_fold_status)
 
-                if finger_tip_y > finger_bottom_y:
-                    fingers.append(0)
-                    print("Fingers with id ",lm_index,"is Closed")
+             #checking if all fingers are folded
+            if all(finger_fold_status):
                 
-                if all(finger_fold_status):
-                    if lm_list[thumb_tip].y < lm_list[thumb_tip-1].y < lm_list[thumb_list-2].y:
-                        print("LIKE")
-                        cv2.putText(img ,"DISLIKE",(20,30), cv2.FONT_HERSHEY_SIMPLEX,1,(0,0),3)
+                # WRITE THE CODE HERE   
 
-                if lm_list[thumb_tip].y < lm_list[thumb_tip-1].y < lm_list[thumb_list-2].y:
-                        print("DISLIKE")
-                        cv2.putText(img ,"DISLIKE",(20,30), cv2.FONT_HERSHEY_SIMPLEX,1,(0,0),3)
+                image = pyautogui.screenshot()
+                image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
+                cv2.imwrite("in_memory_to_disk.png")
 
-             #Code goes here   
+                pyautogui.screenshot("straight_to_disk.png")
+
+
+                image = cv2.imread("straight_to_disk.png")
+                cv2.imshow("Screenshot",imutils.resize(image, width=600))
+
+
+
+
 
 
 
@@ -73,3 +71,9 @@ while True:
 
     cv2.imshow("hand tracking", img)
     cv2.waitKey(1)
+
+
+
+
+
+
